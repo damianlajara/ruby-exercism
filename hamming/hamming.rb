a720553482d52bd428f061deb92969eeef231b85
damianlajara <damiansito93@gmail.com>
2015-07-25 17:02:36 -0400

    succesfully pass all tests

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -1,7 +1,13 @@
 class Hamming
+  VERSION = 1
   def self.compute(strandA,strandB)
-    if strandA == strandB
-      0
-    end
+    # Make sure both of the dna strands are of equal length
+    raise ArgumentError if strandA.length != strandB.length
+    mutations = 0
+    # string#chars returns the string converted to an array
+    # array#zip merges the array with another array given via the parameters and either returns a 2d array with them merged if no block was given, and if there was a block given, then it returns the values of each respective array at that given iteration to yield
+    strandA.chars.zip(strandB.chars) { |dna1, dna2| mutations += 1 if dna1 != dna2 }
+    # Return the amount of mutations between the two DNA strands
+    mutations
   end
 end
